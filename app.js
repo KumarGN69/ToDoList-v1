11d22605e474deb1e5f7c45bbfd93af31be2cd67
KumarGN69 <kumargn1969@hotmail.com>
2022-04-01 02:53:47 +0000

    code changes to post route in ejs file

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const bodyParser = require("body-parser");
 //creting an instance of express app
 const app = express();
 
+//array to hold todo list items
+var listItems = [];
+
 //setting the ejs view engine options in the express app
 app.set("view engine", "ejs");
 
@@ -16,46 +19,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 
-var today = new Date();
-var currDay= today.getDay();
-// console.log(currDay);
-
 //route definitions for GET HTTP method
 app.get("/",function(req,res){
-	// console.log("Hello World!");
-	var day = "";
 	
-	switch(currDay){
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Tuesday";
-			break;	
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			// console.log("Hello "+day);
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-			break;
-		default:
-			console.log("CurrentDay: "+currDay+ "??. Error! There are no more than 7 days in a week");
-		 	break;
-		}
-	// render method for using the ejs file
-	res.render("list",{kindOfDay:day});
+	var today = new Date();
+	
+	var options ={
+		weekDay:"long",
+		day:"numeric",
+		month:"long",
+		year:"numeric"
+	};
+	
+	var day = today.toLocaleDateString("en-US",options);
+	console.log(day);
+	
+	res.render("list",{kindOfDay:day, newListItems:listItems});
 });
 
+//route definition for POSt HTTP method
+app.post("/", function(req, res){
+	
+	listItems.push(req.body.newItem);
+	// console.log(listItem);
+	res.redirect("/");
+});
 //event listener of expresse server
 app.listen(port, function(req,res){
 	console.log("Server started at port 3000!");
